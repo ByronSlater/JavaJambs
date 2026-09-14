5ebce27b68e4ea47390bed6d4e7ad4d91b2fb406
Byron Slater <byron.slater3@googlemail.com>
2026-09-14 12:05:04 +0100

    initial commit with project structure

---
+++ src/main/java/com/javajambs/cher/CherApplication.java
@@ -0,0 +1,13 @@
+package com.javajambs.cher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javajambs/cher/CherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajambs.cher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
