c4a8a96dc056c9264cdd34cffc2e8b188f1662ad
Jennifer McDowell <218159393+JenMcDowell@users.noreply.github.com>
2026-09-15 11:32:41 +0100

    update friends table

--- src/main/resources/db.migration/V4__create_friends_table.sql
+++ src/main/resources/db.migration/V4__create_friends_table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE friends (
     id BIGSERIAL PRIMARY KEY,
-    user1_id integer,
-    user2_id integer,
+    user1_id BIGINT,
+    user2_id BIGINT,
     status varchar(10),
-    created_at timestamp
+    created_at timestamp,
+    FOREIGN KEY (user1_id) REFERENCES users(id),
+    FOREIGN KEY (user2_id) REFERENCES users(id)
 );
